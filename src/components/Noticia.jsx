814bdab42fab11ed9e1dc30d19e45dbc948033d3
Lisandro Civili <lisandrocivili13@gmail.com>
2024-02-18 15:07:16 -0300

    Termino maquetado para sm md y lg. Agrego README

--- src/components/Noticia.jsx
+++ src/components/Noticia.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 const Noticia = ({noticias}) => {
     return (
         <>
         {
             noticias.map((noticia, index)=>(
 
-                <Card className='noticia' key={index}>
-                    <Card.Img></Card.Img>
+                <Card className='noticia text-center' key={index}>
+                    <Card.Img src={noticia.image_url} className='img'></Card.Img>
                     <Card.Body className='cuerpoNoticia'>
                         <Card.Title>{noticia.title}</Card.Title>
                         <Card.Text></Card.Text>
-                        <Button>Ir a la noticia</Button>
                     </Card.Body>
+                    <a href={noticia.source_url} className="btn btn-primary w-50 align-self-center">Ir a la noticia</a>
                 </Card>      
             )
             )
